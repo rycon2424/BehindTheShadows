22a19fd52c61d5d6358d9f3787768559ee31ad93
rycon2424 <rycon2424@users.noreply.github.com>
2018-09-03 13:00:14 +0200

    swapPlatform

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Guard : MonoBehaviour {
-    
 
+    [Header("States/Stats")]
+    public bool alert = false;
+    public bool spottedPlayer = false;
+    public float speed;
 
-	void Start () {
-		
-	}
-	
-	void Update ()
+    [Header("Transforms")]
+    public GameObject playerLocation;
+
+    [Header("Raycast")]
+    public Transform sightStart, sightEnd;
+    public Vector3 sStart, sEnd;
+
+    void Start ()
     {
-		
-	}
+        playerLocation = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        RayCasting();
+        RunTowardsPlayer();
+    }
+
+    void RayCasting()
+    {
+        sStart = sightStart.transform.position;
+        sEnd = sightEnd.transform.position;
+        Debug.DrawLine(sStart, sEnd, Color.green);
+        spottedPlayer = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+    }
+
+    void RunTowardsPlayer()
+    {
+        if (spottedPlayer)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            transform.LookAt(playerLocation.transform.position);
+            transform.Rotate(new Vector3(0, 90, 90), Space.Self);
+        }
+    }
 
 }
