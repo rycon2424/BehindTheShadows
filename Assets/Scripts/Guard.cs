3d65e62bc096d2a8c2584ebfd4c5fdf75fe646c3
rycon2424 <rycon2424@users.noreply.github.com>
2018-09-03 15:48:45 +0200

    Guard Standing Working

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -2,47 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Guard : MonoBehaviour {
+public class Guard : MonoBehaviour
+{
 
     [Header("States/Stats")]
+    public bool standing;
+    public bool walking;
+    [Header(" ")]
     public bool alert = false;
-    public bool spottedPlayer = false;
     public float speed;
+    public float rotateSpeedCalm;
 
-    [Header("Transforms")]
+    [Header("Spots")]
+    public bool spottedPlayer = false;
+
+    [Header("RayCasts")]
+    Ray myRay;
+    RaycastHit hit;
     public GameObject playerLocation;
 
-    [Header("Raycast")]
-    public Transform sightStart, sightEnd;
-    public Vector3 sStart, sEnd;
+    [Header("Alert")]
+    public float rotateSpeed;
+    public float time;
+    bool startTimer = true;      //alertTimer
+    bool canRotate = true;       //If the guard can rotate
 
-    void Start ()
+    void Start()
     {
         playerLocation = GameObject.Find("Player");
     }
 
     void Update()
     {
-        RayCasting();
-        RunTowardsPlayer();
+        RayCast();
+        Alert();
+        if (standing && !alert)
+        {
+            StandingGuard();
+        }
     }
 
-    void RayCasting()
+    #region methods
+
+    void RayCast()
     {
-        sStart = sightStart.transform.position;
-        sEnd = sightEnd.transform.position;
-        Debug.DrawLine(sStart, sEnd, Color.green);
-        spottedPlayer = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+        myRay = new Ray(transform.position, transform.up);
+        Debug.DrawRay(transform.position, transform.up * 30f);
+
+        if (Physics.Raycast(myRay, out hit, 30f))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                StartCoroutine(AlertTimer());
+                spottedPlayer = true;
+            }
+            if (hit.collider.tag == "Player" == false)
+            {
+                spottedPlayer = false;
+            }
+        }
     }
 
-    void RunTowardsPlayer()
+    void Alert()
     {
         if (spottedPlayer)
         {
+            alert = true;
             transform.Translate(Vector3.up * speed * Time.deltaTime);
             transform.LookAt(playerLocation.transform.position);
             transform.Rotate(new Vector3(0, 90, 90), Space.Self);
         }
+        if (!spottedPlayer && alert)
+        {
+            transform.Rotate(new Vector3(0, 0, rotateSpeed));
+            if (startTimer)
+            {
+                StartCoroutine(Timer());
+                startTimer = false;
+            }
+        }
     }
 
+    void StandingGuard()
+    {
+        if (canRotate && !alert)
+        {
+            StartCoroutine(Rotate());
+            transform.Rotate(new Vector3(0, 0, rotateSpeedCalm));
+        }
+    }
+
+    #endregion
+
+    #region IEnums
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSecondsRealtime(time);
+        rotateSpeed = rotateSpeed * -1;
+        startTimer = true;
+    }
+
+    IEnumerator AlertTimer()
+    {
+        yield return new WaitForSecondsRealtime(10);
+        if (alert)
+        {
+            StopCoroutine(AlertTimer());
+        }
+        alert = false;
+    }
+    
+    IEnumerator Rotate()
+    {
+        yield return new WaitForSecondsRealtime(0.9f);
+        canRotate = false;
+        yield return new WaitForSecondsRealtime(0.9f);
+        canRotate = true;
+    }
+    #endregion
 }
