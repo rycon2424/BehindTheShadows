08a24039b2f4eef04c27a72b69f5106da90364c2
rycon2424 <rycon2424@users.noreply.github.com>
2018-09-04 11:31:26 +0200

    Begin minigame

---
+++ Assets/Scripts/UnderShadowMinigame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderShadowMinigame : MonoBehaviour {
+
+    public bool attackCooldown = true;
+    public int whatAttack;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (attackCooldown)
+        {
+            attackCooldown = false;
+            StartCoroutine(Cooldown());
+        }
+	}
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSecondsRealtime(3f);
+        whatAttack = Random.Range(1, 6);
+        attackCooldown = true;
+        Debug.Log("Attack");
+    }
+
+}
