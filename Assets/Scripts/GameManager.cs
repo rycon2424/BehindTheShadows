15a7438c74ebfba41c5074ff7dc369d6fb5f72ff
rycon2424 <rycon2424@users.noreply.github.com>
2018-09-05 12:06:37 +0200

    Minigame can start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static bool Minigame = false;
+    public int whatMinigame;
+    public float minigameDuration;
+
+    public GameObject mainScene;
+    public GameObject[] minigames = new GameObject[2];
+
+    void Update()
+    {
+        if (Minigame)
+        {
+            //whatMinigame = Random.Range(1, 3);
+            whatMinigame = 1;
+            minigames[whatMinigame].SetActive(true);
+            StartCoroutine(Timer());
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        mainScene.SetActive(false);
+        yield return new WaitForSecondsRealtime(minigameDuration);
+        minigames[whatMinigame].SetActive(false);
+        Minigame = false;
+        mainScene.SetActive(true);
+    }
+}
