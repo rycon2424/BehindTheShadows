bddc5f0de7e5e11d81040df66b593e2d5908f2a7
rycon2424 <rycon2424@users.noreply.github.com>
2018-09-07 15:18:52 +0200

    Game Finished

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -6,11 +6,26 @@ using UnityEngine.SceneManagement;
 public class EndScene : MonoBehaviour
 {
 
-	void Update ()
+    public bool canSkip = false;
+
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0))
+        StartCoroutine(ResetGame());
+    }
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0) && canSkip)
         {
+            GameManager.playerHP = 5;
             SceneManager.LoadScene(0);
         }
 	}
+
+    IEnumerator ResetGame()
+    {
+        yield return new WaitForSecondsRealtime(8);
+        canSkip = true;
+
+    }
 }
